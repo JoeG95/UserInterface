dac880e14c6a3ee7edaf5a4e9ce9c9c31d68465e
Gomez Lira <gomejl@AEESINC.COM>
2020-10-23 10:26:01 -0500

    Phase Number One

---
+++ PKC_App.py
@@ -0,0 +1,100 @@
+from tkinter import *
+from tkinter import messagebox as MessageBox
+
+app = Tk()
+app.minsize(1880,1010)
+app.resizable(0,0)
+app.title('Pick to Light System')
+app.config(bg = 'light blue', bd = 15, relief = 'groove')
+app.iconbitmap('HARNESS ENGINE.ico')
+
+#------------------------------------------------------Functions--------------------------------------------------------
+
+def operationA():
+    MessageBox.showinfo('Connector A', 'Starting sequence')
+    print('Starting')
+
+def operationB():
+    MessageBox.showinfo('Connector B', 'Starting sequence')
+
+def close():
+    result = MessageBox.askquestion('Exit', 'Do you want to leave?')
+    if result == 'yes':
+        MessageBox.showinfo('Good bye', 'Finishing test')
+        app.destroy()
+    else:
+        MessageBox.showinfo('Stay', 'Process will conitnue')
+
+#------------------------------------------------------Pictures---------------------------------------------------------
+
+picA = PhotoImage(file = "Connector1.png")
+picB = PhotoImage(file = "Connector2.png")
+
+#--------------------------------------------------------Labels---------------------------------------------------------
+
+title = Label(app, text = "Please Select a Connector Part Number: ")
+title.config(
+    bg = "light blue",
+    fg = "black",
+    font = ("Arial Black", 20)
+)
+title.grid(row = 0, column = 0, columnspan = 12, padx = 0, pady = 0)
+
+ConA = Label(app, image = picA)
+ConA.grid(
+    row = 1,
+    column = 0,
+    padx = 0,
+    pady = 0
+)
+
+ConB= Label(app, image = picB)
+ConB.grid(
+    row = 1,
+    column = 1,
+    padx = 0,
+    pady = 0
+)
+
+infoD= Label(app, text = "Oracle Number: 199022")
+infoD.config(
+    bg = "light blue",
+    fg = "black", font = ("Arial Black", 12)
+)
+infoD.grid(row = 2, column = 0, padx = 0, pady = 0)
+
+infoE= Label(app, text = "Oracle Number: 970381")
+infoE.config(
+    bg = "light blue",
+    fg = "black",
+    font = ("Arial Black", 12)
+)
+infoE.grid(row = 2, column = 1, padx = 0, pady = 0)
+
+#--------------------------------------------------------Buttons--------------------------------------------------------
+
+optA = Button(app, text = "First Connector", width = 30, height = 5, command = operationA)
+optA.config(
+    bg = "light yellow",
+    fg = "black",
+    font = ("Arial Black", 14)
+)
+optA.grid(row = 3, column = 0, padx = 0, pady = 0, ipadx = 0, ipady = 0)
+
+optB = Button(app, text = "Second Connector", width = 30, height = 5, command = operationB)
+optB.config(
+    bg = "light yellow",
+    fg = "black",
+    font = ("Arial Black", 14)
+)
+optB.grid(row = 3, column = 1, padx = 0, pady = 0, ipadx = 0, ipady = 0)
+
+optC = Button(app, text = "Close App", width = 30, height = 5, command = close)
+optC.config(
+    bg = "light yellow",
+    fg = "black",
+    font = ("Arial Black", 14)
+)
+optC.grid(row = 4, column = 0, padx = 0, pady = 0, ipadx = 0, ipady = 0)
+
+app.mainloop()
